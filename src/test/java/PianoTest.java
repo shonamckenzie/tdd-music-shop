2135349335fbeab8672e1f6da04621cde0962cf5
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-20 18:56:18 +0100

    added tests for piano and trumpet

--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,2 +1,41 @@
+import instruments.InstrumentType;
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before(){
+        piano = new Piano("wood", "black", InstrumentType.keyboard, 800, 1150);
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("wood", piano.getMaterial());
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("black", piano.getColour());
+    }
+
+    @Test
+    public void hasInstrumentType() {
+        assertEquals(InstrumentType.keyboard, piano.getType());
+    }
+
+    @Test
+    public void hasBuyPrice() {
+        assertEquals(800, piano.getBuyPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellPrice() {
+        assertEquals(1150, piano.getSellPrice(), 0.01);
+    }
 }
--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -1,2 +1,41 @@
+import instruments.InstrumentType;
+import instruments.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void before(){
+        trumpet = new Trumpet("brass", "gold", InstrumentType.brass, 500, 650);
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("brass", trumpet.getMaterial());
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("gold", trumpet.getColour());
+    }
+
+    @Test
+    public void hasInstrumentType() {
+        assertEquals(InstrumentType.brass, trumpet.getType());
+    }
+
+    @Test
+    public void hasBuyPrice() {
+        assertEquals(500, trumpet.getBuyPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellPrice() {
+        assertEquals(650, trumpet.getSellPrice(), 0.01);
+    }
 }
